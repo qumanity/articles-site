2e546e4e192f4dc09de8a5b8e2f8d4e6e57be6ee
qumanity <n.ivanov.official@gmail.com>
2024-11-21 23:32:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -16,18 +18,12 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # Модели пользователей
-class User(UserMixin):
-    def __init__(self, id):
-        self.id = id
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
 
-# Статический пользователь для тестирования
-users = {'admin': {'password': 'password'}}
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User(user_id)
-
-# Модель статьи
+# Статья
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -36,25 +32,44 @@ class Article(db.Model):
     def __repr__(self):
         return f"Article('{self.title}', '{self.content[:20]}')"
 
-# Создание админки
-class AdminView(ModelView):
-    def is_accessible(self):
-        return current_user.is_authenticated and current_user.id == 'admin'
+# Форма для редактирования статьи с CKEditor
+class ArticleForm(FlaskForm):
+    title = StringField('Title')
+    content = TextAreaField('Content')
+
+# Интеграция CKEditor в форму через админку
+class ArticleView(ModelView):
+    form = ArticleForm
+
+    # Подключаем CKEditor в форме
+    def on_model_change(self, form, model, is_created):
+        model.content = form.content.data
+
+    def _get_extra_css(self):
+        return super()._get_extra_css() + """
+        <link rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/34.2.0/classic/theme.css">
+        """
+
+    def _get_extra_js(self):
+        return super()._get_extra_js() + """
+        <script src="https://cdn.ckeditor.com/ckeditor5/34.2.0/classic/ckeditor.js"></script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                ClassicEditor
+                    .create(document.querySelector('#content'))
+                    .catch(error => {
+                        console.error(error);
+                    });
+            });
+        </script>
+        """
 
 admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
-admin.add_view(AdminView(Article, db.session))
+admin.add_view(ArticleView(Article, db.session))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username in users and users[username]['password'] == password:
-            user = User(username)
-            login_user(user)
-            return redirect('/admin')
-        else:
-            return 'Неверные учетные данные', 401
+    # Логика для логина (пока простая)
     return render_template('login.html')
 
 @app.route('/logout')
@@ -65,3 +80,4 @@ def logout():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
