54ff9fa5dd75645d88f22d31f2591b74ff97bd11
qumanity <n.ivanov.official@gmail.com>
2024-11-23 03:05:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -16,14 +18,21 @@ db = SQLAlchemy(app)
 # Настройка Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-# Модели пользователей
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# Модели
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
 
-# Статья
+
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -32,45 +41,45 @@ class Article(db.Model):
     def __repr__(self):
         return f"Article('{self.title}', '{self.content[:20]}')"
 
-# Форма для редактирования статьи с CKEditor
+
+# Форма
 class ArticleForm(FlaskForm):
-    title = StringField('Title')
-    content = TextAreaField('Content')
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Save')
 
-# Интеграция CKEditor в форму через админку
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+# Админка
 class ArticleView(ModelView):
-    form = ArticleForm
+    def is_accessible(self):
+        return current_user.is_authenticated
 
-    # Подключаем CKEditor в форме
-    def on_model_change(self, form, model, is_created):
-        model.content = form.content.data
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect('/login')
 
-    def _get_extra_css(self):
-        return super()._get_extra_css() + """
-        <link rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/34.2.0/classic/theme.css">
-        """
 
-    def _get_extra_js(self):
-        return super()._get_extra_js() + """
-        <script src="https://cdn.ckeditor.com/ckeditor5/34.2.0/classic/ckeditor.js"></script>
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                ClassicEditor
-                    .create(document.querySelector('#content'))
-                    .catch(error => {
-                        console.error(error);
-                    });
-            });
-        </script>
-        """
-
-admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
+admin = Admin(app, name='Admin Panel', template_mode='bootstrap4')
 admin.add_view(ArticleView(Article, db.session))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Логика для логина (пока простая)
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect('/admin')
+        else:
+            flash('Invalid credentials')
+    return render_template('login.html', form=form)
+
 
 @app.route('/logout')
 @login_required
@@ -78,6 +87,27 @@ def logout():
     logout_user()
     return redirect('/')
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
+@app.route('/articles')
+def articles():
+    articles = Article.query.all()
+    return render_template('articles.html', articles=articles)
+
+
+@app.route('/articles/edit/<int:article_id>', methods=['GET', 'POST'])
+@login_required
+def edit_article(article_id):
+    article = Article.query.get_or_404(article_id)
+    form = ArticleForm(obj=article)
+    if form.validate_on_submit():
+        article.title = form.title.data
+        article.content = form.content.data
+        db.session.commit()
+        flash('Article updated successfully!')
+        return redirect(url_for('articles'))
+    return render_template('edit_article.html', form=form, article=article)
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
